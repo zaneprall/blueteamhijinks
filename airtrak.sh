76d264c6da4d2cf7bc38a0cfdc86d8f060bb5d66
Zane Prall <99636457+zaneprall@users.noreply.github.com>
2023-03-14 09:55:55 -0600

    Add files via upload

---
+++ airtrak.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# log file path
+log_file="/var/log/discovered_devices.log"
+
+
+if [ ! -f "$log_file" ]; then
+    touch "$log_file"
+fi
+
+
+while true; do
+
+    echo -e 'power on\nscan on' | bluetoothctl > /dev/null &
+    sleep 10
+    echo -e 'scan off\nquit' | bluetoothctl > /dev/null
+
+
+    if ls /var/lib/bluetooth/*/cache/*.cache > /dev/null 2>&1; then
+        grep -oP 'Device \K\w{2}:\w{2}:\w{2}:\w{2}:\w{2}:\w{2}' /var/lib/bluetooth/*/cache/*.cache | sort -u | while read -r line; do
+
+            if grep -q "$line" "$log_file"; then
+                continue
+            fi
+
+
+            name=$(bluetoothctl info "$line" | grep -oP 'Name: \K.*')
+            rssi=$(bluetoothctl info "$line" | grep -oP 'RSSI: \K.*')
+            txrate=$(bluetoothctl info "$line" | grep -oP 'Tx Power: \K.*')
+            echo "$(date +'%Y-%m-%d %H:%M:%S') $line $name (RSSI: $rssi dBm, Tx Power: $txrate dBm)" >> "$log_file"
+        done
+    fi
+
+    # Wait for 10
+    sleep 10
+done
